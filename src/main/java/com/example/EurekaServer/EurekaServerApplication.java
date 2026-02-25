65e052138832a9bcec6151a3b71f2234c258a727
Arnab Saha <arnabsaha2506@gmail.com>
2026-02-25 13:53:07 +0530

    Updated Eureka Server and set up the server to use

--- src/main/java/com/example/EurekaServer/EurekaServerApplication.java
+++ src/main/java/com/example/EurekaServer/EurekaServerApplication.java
@@ -1,12 +1,18 @@
 package com.example.EurekaServer;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
